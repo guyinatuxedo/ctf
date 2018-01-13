e48064a90d41cc20a579b440e4bb383d39357469
Ryan <ryancmeinke@gmail.com>
2018-01-13 16:15:59 -0500

    Add files via upload

---
+++ other/rev/movfuscated1/reverent.py
@@ -0,0 +1,20 @@
+#Import the libraries
+from subprocess import *
+import string
+import sys
+
+#Establish the command to count the number of instructions
+command = "perf stat -x : -e instructions:u " + sys.argv[1] + " 1>/dev/null" 
+flag = ''
+while True:
+	ins_count = 0
+	count_chr = ''
+	for i in string.printable:
+		target = Popen(command, stdout=PIPE, stdin=PIPE, stderr=STDOUT, shell=True)
+		target_output, _ = target.communicate(input='%s\n'%(flag + i))
+		instructions = int(target_output.split(':')[0])
+		if instructions > ins_count:
+			count_chr = i
+			ins_count = instructions
+	flag += count_chr
+	print flag
